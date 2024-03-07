dcb9a3f5d841f681379c1c6895bbd858aff029a7
Mederis Frambourt <mframbou@student.42nice.fr>
2024-03-08 00:46:29 +0100

    grace done python easy

---
+++ Python/Grace/Grace.py
@@ -0,0 +1,5 @@
+# Squalala
+
+s = "# Squalala{0}{0}s = {1}{2}{1}{0}with open('Grace_kid.py', 'w') as f:{0}    f.write(s.format(chr(10), chr(34), s)){0}"
+with open('Grace_kid.py', 'w') as f:
+    f.write(s.format(chr(10), chr(34), s))
