51a216e0c464312b4aa9dc386d69271c1282996c
Mederis Frambourt <mframbou@student.42nice.fr>
2024-03-09 02:23:08 +0100

    Sully done in ASM :D all done

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -3,14 +3,14 @@ extern dprintf
 extern open
 extern close
 extern strcmp
-extern printf
 extern sprintf
+extern system
 
 section .rodata
-  msg: db "global main%1$cextern dprintf%1$cextern open%1$cextern close%1$cextern strcmp%1$cextern sprintf%1$c%1$csection .rodata%1$c  msg: db %2$c%3$s%2$c, 0%1$c  sully_base_filename: db %2$cSully.s%2$c, 0%1$c  filename_template: db %2$cSully_%%d.s%2$c, 0%1$c  cmd_template: db %2$cnasm -f elf64 Sully_%%1$d && ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o -lc Sully_%%1$d && rm Sully_%%1$d.o && ./Sully_%%1$d%2$c, 0%1$c  current_filename: db __FILE__, 0%1$c%1$csection .text%1$c_decrement_number:%1$c  mov rax, [number]%1$c  sub rax, 1%1$c  mov [number], al%1$c  ret%1$c%1$c; sets the right number in the variable number (i-1 if needed else i)%1$cset_right_number:%1$c  mov rdi, current_filename%1$c  mov rsi, sully_base_filename%1$c  call strcmp%1$c  cmp rax, 0%1$c  jne _decrement_number%1$c  ret%1$c%1$c_exit:%1$c  mov rax, 0%1$c  ret%1$c%1$ccreate_filename:%1$c  mov rdi, out_filename%1$c  mov rsi, filename_template%1$c  movzx rdx, byte [number] ; mov with zero extend (since our int is only 1 byte)%1$c  mov rax, 0%1$c  call sprintf%1$c  ret%1$c%1$ccreate_cmd:%1$c  mov rdi, cmd%1$c  mov rsi, cmd_template%1$c  movzx rdx, byte [number]%1$c  mov rax, 0%1$c  call sprintf%1$c  ret%1$c%1$cmain:%1$c  cmp byte [number], 0%1$c  jbe _exit%1$c%1$c  call set_right_number%1$c%1$c  call create_filename%1$c  call create_cmd%1$c%1$c  mov rdi, out_filename%1$c  mov rsi, 578%1$c  mov rdx, 420%1$c  call open%1$c%1$c  mov rdi, rax%1$c  mov rsi, msg%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  mov r8, msg%1$c  movzx r9, byte [number]%1$c  push r9 ; arg should be in r9 but from testing it must be in the stack%1$c  mov rax, 0%1$c  call dprintf%1$c  pop r9%1$c  call close%1$c%1$c  mov rax, 0%1$c  ret%1$c%1$csection .data%1$c  ; this is our int%1$c  number: db %5$d%1$c  ; our arrays (https://stackoverflow.com/questions/29545696/declaring-arrays-in-x86-assembly)%1$c  out_filename: TIMES 50 db 0%1$c  cmd: TIMES 300 db 0%1$c%1$c", 0
+  msg: db "global main%1$cextern dprintf%1$cextern open%1$cextern close%1$cextern strcmp%1$cextern sprintf%1$cextern system%1$c%1$csection .rodata%1$c  msg: db %2$c%3$s%2$c, 0%1$c  sully_base_filename: db %2$cSully.s%2$c, 0%1$c  filename_template: db %2$cSully_%%d.s%2$c, 0%1$c  cmd_template: db %2$cnasm -f elf64 Sully_%%1$d.s && ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o -lc Sully_%%1$d.o -o Sully_%%1$d && rm Sully_%%1$d.o && ./Sully_%%1$d%2$c, 0%1$c  current_filename: db __FILE__, 0%1$c%1$csection .text%1$c_decrement_number:%1$c  mov rax, [number]%1$c  sub rax, 1%1$c  mov [number], al%1$c  ret%1$c%1$c; sets the right number in the variable number (i-1 if needed else i)%1$cset_right_number:%1$c  mov rdi, current_filename%1$c  mov rsi, sully_base_filename%1$c  call strcmp%1$c  cmp rax, 0%1$c  jne _decrement_number%1$c  ret%1$c%1$c_exit:%1$c  mov rax, 0%1$c  ret%1$c%1$ccreate_filename:%1$c  mov rdi, out_filename%1$c  mov rsi, filename_template%1$c  movzx rdx, byte [number] ; mov with zero extend (since our int is only 1 byte)%1$c  mov rax, 0%1$c  call sprintf%1$c  ret%1$c%1$ccreate_cmd:%1$c  mov rdi, cmd%1$c  mov rsi, cmd_template%1$c  movzx rdx, byte [number]%1$c  mov rax, 0%1$c  call sprintf%1$c  ret%1$c%1$cmain:%1$c  cmp byte [number], 0%1$c  jbe _exit%1$c%1$c  call set_right_number%1$c%1$c  call create_filename%1$c  call create_cmd%1$c%1$c  mov rdi, out_filename%1$c  mov rsi, 578%1$c  mov rdx, 420%1$c  call open%1$c%1$c  mov rdi, rax%1$c  mov rsi, msg%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  mov r8, msg%1$c  movzx r9, byte [number]%1$c  push r9 ; arg should be in r9 but from testing it must be in the stack%1$c  mov rax, 0%1$c  call dprintf%1$c  pop r9%1$c  call close%1$c%1$c; need to save stack pointer (thanks gcc -S for debugging)%1$c  mov rdi, cmd%1$c  mov rax, 0%1$c  push rbp%1$c  call system%1$c  pop rbp%1$c%1$c  mov rax, 0%1$c  ret%1$c%1$csection .data%1$c  ; this is our int%1$c  number: db %5$d%1$c  ; our arrays (https://stackoverflow.com/questions/29545696/declaring-arrays-in-x86-assembly)%1$c  out_filename: TIMES 50 db 0%1$c  cmd: TIMES 300 db 0%1$c", 0
   sully_base_filename: db "Sully.s", 0
   filename_template: db "Sully_%d.s", 0
-  cmd_template: db "nasm -f elf64 Sully_%1$d && ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o -lc Sully_%1$d.o -o Sully_%1$d && rm Sully_%1$d.o && true || ./Sully_%1$d", 0
+  cmd_template: db "nasm -f elf64 Sully_%1$d.s && ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o -lc Sully_%1$d.o -o Sully_%1$d && rm Sully_%1$d.o && ./Sully_%1$d", 0
   current_filename: db __FILE__, 0
 
 section .text
@@ -59,14 +59,6 @@ main:
   call create_cmd
 
   mov rdi, out_filename
-  mov rax, 0
-  call printf
-
-  mov rdi, cmd
-  mov rax, 0
-  call printf
-
-  mov rdi, out_filename
   mov rsi, 578
   mov rdx, 420
   call open
@@ -82,6 +74,13 @@ main:
   call dprintf
   pop r9
   call close
+
+; need to save stack pointer (thanks gcc -S for debugging)
+  mov rdi, cmd
+  mov rax, 0
+  push rbp
+  call system
+  pop rbp
 
   mov rax, 0
   ret
