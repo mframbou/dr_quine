646c5e02d426c4cb45bbe13d4b7a1f6bac8063be
Mederis Frambourt <mframbou@c2r3p2.42nice.fr>
2024-03-12 11:55:26 +0100

    add Makefile for python quines + fix sully filename check

--- Python/Sully/Sully.py
+++ Python/Sully/Sully.py
@@ -1,12 +1,13 @@
-from os import system
+from os import system, path
 
-get_right_i = lambda i: 5 if __file__ == "Sully.py" else i-1
+get_right_i = lambda i: 5 if path.basename(__file__) == "Sully.py" else i - 1
 
-i = 5;
-if i <= 0: exit(0)
+i = 5
+if i <= 0:
+    exit(0)
 filename = "Sully_%d.py" % get_right_i(i)
 cmd = "python3 Sully_%d.py" % get_right_i(i)
 with open(filename, "w") as f:
-    s = "from os import system{0}{0}get_right_i = lambda i: 5 if __file__ == {1}Sully.py{1} else i-1{0}{0}i = {3};{0}if i <= 0: exit(0){0}filename = {1}Sully_%d.py{1} % get_right_i(i){0}cmd = {1}python3 Sully_%d.py{1} % get_right_i(i){0}with open(filename, {1}w{1}) as f:{0}    s = {1}{2}{1}{0}    f.write(s.format((chr(10)), chr(34), s, get_right_i(i))){0}system(cmd){0}"
+    s = "from os import system, path{0}{0}get_right_i = lambda i: 5 if path.basename(__file__) == {1}Sully.py{1} else i - 1{0}{0}i = {3}{0}if i <= 0:{0}    exit(0){0}filename = {1}Sully_%d.py{1} % get_right_i(i){0}cmd = {1}python3 Sully_%d.py{1} % get_right_i(i){0}with open(filename, {1}w{1}) as f:{0}    s = {1}{2}{1}{0}    f.write(s.format((chr(10)), chr(34), s, get_right_i(i))){0}system(cmd){0}"
     f.write(s.format((chr(10)), chr(34), s, get_right_i(i)))
 system(cmd)
